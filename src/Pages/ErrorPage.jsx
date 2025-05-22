9e5dab20d24e26e1dcbfcfcb034c37d1f565c13d
sourav-dn <souravdn.dev@gmail.com>
2025-05-23 01:04:30 +0600

    give errorpage a back home button

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,11 +1,17 @@
 import Lottie from "lottie-react";
 import notFoundPage from "../assets/Lottie/404-animation.json"
+import { Link } from "react-router";
 
 const ErrorPage = () => {
     return (
-        <div>
-            <Lottie animationData={notFoundPage} loop={true} />
-        </div>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-white text-center px-4">
+      <div className="max-w-lg w-full">
+        <Lottie animationData={notFoundPage} loop={true} />
+        <Link to="/">
+          <button className="btn btn-primary">Go Back Home</button>
+        </Link>
+      </div>
+    </div>
     );
 };
 
