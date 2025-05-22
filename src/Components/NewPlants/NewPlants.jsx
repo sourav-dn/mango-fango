7d28fb94e254c98f93586e963a02a1142f201138
sourav-dn <souravdn.dev@gmail.com>
2025-05-22 22:29:05 +0600

    newplants responsive for mobile,medium and large devices

--- src/Components/NewPlants/NewPlants.jsx
+++ src/Components/NewPlants/NewPlants.jsx
@@ -9,12 +9,12 @@ const NewPlants = () => {
       .then((res) => res.json())
       .then((data) => setPlants(data))
       .catch((err) => console.error("Fetch Error:", err));
-        // .catch(err => console.error(err));
+        
   }, []);
     return (
-        <div>
-            <h1 className='text-4xl font-bold text-center mt-5 mb-5'>New Plants</h1>
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
+        <div className="container mx-auto px-4 md:px-6 lg:px-8 py-8">
+            <h1 className='text-2xl sm:text-3xl md:text-4xl font-bold text-center mt-5 mb-5'>New Plants</h1>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
                 {
                     plants.map(plant => <NewPlantsCard key={plant.name} plant={plant}></NewPlantsCard>)
                 }
